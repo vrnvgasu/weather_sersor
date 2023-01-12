aa192b8346e87ba9aeec05ec57f65fc65e7d6209
dmitrii sh <dmitrii@sh.com>
2023-01-12 23:19:14 +0300

    init

---
+++ src/main/java/ru/edu/weather_sensor/WeatherSensorApplication.java
@@ -0,0 +1,13 @@
+package ru.edu.weather_sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSensorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WeatherSensorApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/edu/weather_sensor/WeatherSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.edu.weather_sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSensorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
